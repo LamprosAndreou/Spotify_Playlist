72c9511770706cf8d6768f233a1d438e38e8b686
Lampros Andreou <148083769+LamprosAndreou@users.noreply.github.com>
2024-08-06 11:05:13 +0300

    Update Spotify.js

--- Spotify-Playlist/src/util/Spotify.js
+++ Spotify-Playlist/src/util/Spotify.js
@@ -1,5 +1,5 @@
-const clientId = ''; // Insert client ID here.
-const redirectUri = 'http://localhost:3000/'; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
+const clientId = ''; 
+const redirectUri = 'http://localhost:3000/'; 
 let accessToken;
 
 const Spotify = {
@@ -14,7 +14,7 @@ const Spotify = {
       accessToken = accessTokenMatch[1];
       const expiresIn = Number(expiresInMatch[1]);
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
-      window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
+      window.history.pushState('Access Token', null, '/'); 
       return accessToken;
     } else {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
